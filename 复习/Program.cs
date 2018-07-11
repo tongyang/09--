b1a0849aa2932f7d946214b260cdf17476be55ce
扬 童 <452949529@qq.com>
2018-07-11 16:39:11 +0800

    命名空间

--- 复习/Program.cs
+++ 复习/Program.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 复习;
 
-namespace _09_面向对象继承
+namespace 复习
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Person p = new Person();
         }
     }
 }
