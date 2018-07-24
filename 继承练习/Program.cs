5ffa1ed27a84e312cf24e6b054a322e1b08540a1
扬 童 <452949529@qq.com>
2018-07-24 11:11:08 +0800

    里式转换

--- 继承练习/Program.cs
+++ 继承练习/Program.cs
@@ -13,8 +13,21 @@ namespace 继承练习
             Reporter rep = new Reporter(30, "童扬", "打篮球");
             rep.SayHello(rep.Name, rep.Habbit);
 
-            Driver dr = new Driver("劳改", 19);
+            Person p = new Driver("to", 12);
             
+
+            // is与as的用法
+            if (p is Reporter)
+            {
+                Reporter r = (Reporter)p;
+            }
+            else
+            {
+                Console.WriteLine("转换失败");
+            }
+
+            Driver d = p as Driver;
+
             Console.ReadKey();
         }
     }
