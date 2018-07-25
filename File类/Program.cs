06b428c17aad2947394bb0acc8d68023e04ba1df
扬 童 <452949529@qq.com>
2018-07-25 16:41:11 +0800

    File与Path类

---
+++ File类/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File类
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestPath();
+            TestFile();
+        }
+
+        public static void TestFile()
+        {
+            string path = Path.GetFullPath(@"C:\Users\45294\Desktop\1.txt");
+            //File.Create(path);
+            //File.Delete(path);
+            //File.Copy(path, Path.Combine(Path.GetDirectoryName(path), "2.txt"));
+            File.WriteAllText(path, "这是第一次写入adadas", Encoding.UTF8);
+        }
+
+        public static void TestPath()
+        {
+            string str = @"D:\教程\基础\视频\亲情奉献全套精品.Net基础视频教程之9-面向对象继承\（第十一天）\video\9、Path类.avi";
+            Console.WriteLine(Path.GetFileName(str)); // 获得文件的名称，包括文件的扩展名
+            Console.WriteLine(Path.GetFileNameWithoutExtension(str)); // 获得文件的名称，不包括文件的扩展名
+            Console.WriteLine(Path.GetExtension(str)); // 获得文件的扩展名
+            Console.WriteLine(Path.GetDirectoryName(str)); // 获得文件所在的目录（递归）
+            Console.WriteLine(Path.GetFullPath(str)); // 获得文件的全路径
+            // 拼接文件路径
+            Console.WriteLine(Path.Combine(@"D:\教程\基础\视频\亲情奉献全套精品.Net基础视频教程之9-面向对象继承\（第十一天）\video\", @"9、Path类.avi"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HashTable集合/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashTable集合
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("姓名", "童扬");
+            ht.Add("性别", '男');
+            ht.Add("年龄", 29);
+            ht.Add("爱好", "篮球");
+
+            if (ht.ContainsKey("年级"))
+            {
+                ht["年级"] = "三年级";
+            }
+            else
+            {
+                ht.Add("年级", "三年级");
+            }
+
+            foreach (var item in ht.Keys)
+            {
+                Console.WriteLine(ht[item]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
